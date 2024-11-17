9c675d1d705b4c25ff4c875e6f4b5e4c5eb699b9
Simi <adegokesimi@gmail.com>
2024-11-17 18:39:31 +0100

    reformatting

--- src/services/rule_engine_service.py
+++ src/services/rule_engine_service.py
@@ -12,9 +12,10 @@ logger = logging.getLogger(__name__)
 
 executor = ThreadPoolExecutor(max_workers=1)
 
+
 class RuleEngine:
     def __init__(self):
-        self.db:DatabaseManager = g.db_manager
+        self.db: DatabaseManager = g.db_manager
         self.conditional_map = {
             "GreaterThan": lambda x, y: x > y,
             "LessThan": lambda x, y: x < y,
@@ -24,30 +25,30 @@ class RuleEngine:
             "NotEqualTo": lambda x, y: x != y
         }
         self.type_validation_map = {
-                'float': lambda v: float(v),
-                'int': lambda v: int(v),
-                'datetime': lambda v: datetime.strptime(v, "%Y-%m-%d %H:%M:%S"),
-                'vachar': lambda v:str(v),
-                'string': lambda v:str(v),
-                'str': lambda v:str(v),
-            }
+            'float': lambda v: float(v),
+            'int': lambda v: int(v),
+            'datetime': lambda v: datetime.strptime(v, "%Y-%m-%d %H:%M:%S"),
+            'vachar': lambda v: str(v),
+            'string': lambda v: str(v),
+            'str': lambda v: str(v),
+        }
 
     def get_conditionals(self):
         return [key for key in self.conditional_map]
-    
+
     def __get_table_columns(self, table_name):
         table_columns = self.db.get_columns_of_table(table_name)
         if table_columns:
             return {row[0]: row[1] for row in table_columns}
         return None
-    
+
     def get_data_points(self):
-        try: 
+        try:
             table_columns = self.__get_table_columns('kd_hk_transactions')
             if table_columns:
                 data = {
-                    'datapoints': table_columns, 
-                    'conditionals': self.get_conditionals(), 
+                    'datapoints': table_columns,
+                    'conditionals': self.get_conditionals(),
                     'category': ['transactions']
                 }
                 return ResponseDto(True, 'success', data, 200)
@@ -56,13 +57,13 @@ class RuleEngine:
         except Exception as e:
             logger.error(f"Error fetching table columns: {e}")
             return ResponseDto(False, 'An error occurred while fetching data points', None, 500)
-        
-    def __keys_exist(self, data:dict, keys:list):
+
+    def __keys_exist(self, data: dict, keys: list):
         return all(key in data and data[key] for key in keys)
-    
+
     def __convert_keys_to_lowercase(self, input_dict):
         return {k.lower(): v for k, v in input_dict.items()}
-    
+
     def __save_report(self, rule_id, data, saving_count=0):
         insert_query = """
                     insert into kd_hk_report
@@ -74,7 +75,7 @@ class RuleEngine:
             self.db.single_inserts(insert_query, (rule_id, json.dumps(data)))
         except Exception as e:
             logger.error(f"Failed to insert report for rule_id {rule_id}: {e}")
-    
+
     def __validate_value_type_rule(self, rule, data):
         """
         Validates a rule against the given data.
@@ -92,18 +93,21 @@ class RuleEngine:
 
         # Guard clause: Ensure the column exists in the data
         if column_to_check not in data:
-            logger.warning(f"Data column '{column_to_check}' not present in the request")
+            logger.warning(f"Data column '{
+                           column_to_check}' not present in the request")
             return False
 
         try:
             # Convert values
             converter = self.type_validation_map.get(check_value_data_type)
             if not converter:
-                logger.error(f"Unsupported data type for conversion: {check_value_data_type}")
+                logger.error(f"Unsupported data type for conversion: {
+                             check_value_data_type}")
                 return False
-            
+
             converted_check_value = converter(data[column_to_check])
-            converted_value_to_check_against = converter(value_to_check_against)
+            converted_value_to_check_against = converter(
+                value_to_check_against)
 
             # Evaluate the condition
             is_faulted = self.conditional_map[conditional](
@@ -123,9 +127,9 @@ class RuleEngine:
 
     def __validate_expression_type_rule(self, rule, data):
         return False
-    
+
     def __validate_rule(self, rule, data):
-        #get rule type
+        # get rule type
         # isExpression or not
         if not rule[2]:
             return self.__validate_value_type_rule(rule, data)
@@ -139,7 +143,7 @@ class RuleEngine:
         dataPoint = dataRequest['dataPoint']
         checkValue = dataRequest['checkValue']
         conditional = dataRequest['conditional']
-        
+
         if conditional not in self.conditional_map:
             return ResponseDto(False, f"Unsupported conditional: {conditional}", None, 400)
         table_columns = self.__get_table_columns('kd_hk_transactions')
@@ -148,16 +152,13 @@ class RuleEngine:
             return ResponseDto(False, 'dataPoint is not mapped to the table', None, 400)
 
         column_data_type = table_columns[dataPoint]
-        
+
         if 'char' not in column_data_type:
             if column_data_type in self.type_validation_map:
                 try:
                     self.type_validation_map[column_data_type](checkValue)
                 except ValueError:
-                    return ResponseDto(False,
-                                       f'Invalid data type for checkValue. Expected {
-                                           column_data_type}.',
-                                       None, 400)
+                    return ResponseDto(False, f'Invalid data type for checkValue. Expected {column_data_type}.', None, 400)
         description = dataRequest['description'] if dataRequest['description'] else ''
         insert_query = """
             INSERT INTO kd_hk_rules
@@ -170,9 +171,9 @@ class RuleEngine:
             return ResponseDto(False, 'Error creating rule. Please try again later.', None, 400)
 
         return ResponseDto(True, 'Rule has been set successfully', None, 200)
-    
+
     def rule_check(self, data) -> ResponseDto:
-        try: 
+        try:
             select_rules_query = "select * from kd_hk_rules with(nolock) where isActive = 1"
             active_rules = self.db.fetch_records(select_rules_query, ())
             if active_rules:
@@ -188,7 +189,7 @@ class RuleEngine:
         except Exception as e:
             logger.error(f"Error occurred during rule check: {e}")
             return ResponseDto(False, 'An error occured', False, 500)
-    
+
     def get_rules(self) -> List[dict]:
         try:
             select_rules_query = "select * from kd_hk_rules with(nolock) where isActive = 1"
@@ -197,7 +198,7 @@ class RuleEngine:
             if active_rules:
                 for rule in active_rules:
                     rule_dict = {}
-                    rule_dict['type']  = 'Expression' if rule[2] else 'ValueCheck'
+                    rule_dict['type'] = 'Expression' if rule[2] else 'ValueCheck'
                     rule_dict['description'] = rule[9]
                     rule_dict['id'] = rule[0]
                     results.append(rule_dict)
@@ -207,7 +208,7 @@ class RuleEngine:
         except Exception as e:
             logger.error(f'error_trying_to_get_rules {e}')
             return ResponseDto(False, 'An error occured', False, 500)
-    
+
     def get_report(self) -> List[dict]:
         try:
             select_report_query = """
@@ -222,7 +223,7 @@ class RuleEngine:
             if records:
                 for index, record in enumerate(records):
                     results.append({
-                        'sn':index+1,
+                        'sn': index+1,
                         'payloadType': record[0],
                         'payloadDetails': json.loads(record[1]),
                         'date': record[2],
@@ -234,12 +235,14 @@ class RuleEngine:
             logger.error(f'error_trying_to_get_report {e}')
             return ResponseDto(False, 'An error occured', False, 500)
 
-            
-    def set_expression_type_rule(self, dataRequest:dict):
+    def set_expression_type_rule(self, dataRequest: dict):
         try:
             if not self.__keys_exist(dataRequest, ['dataPoint', 'expression', 'conditional']):
                 return ResponseDto(False, 'Invalid request: Missing or empty values', None, 400)
-            return 
-        except Exception as e:
-            return 
 
+            # check if the expression is valid
+            # run the script and get the first result and insert to the expression db
+            # set a database trigger for the rule
+            return
+        except Exception as e:
+            return
